35f63e659db26841d118f712ed3cb6990e54fac4
Navneeth <navneetsasikumar@gmail.com>
2023-12-18 10:28:52 +0530

    "cpu inference"

--- test.py
+++ test.py
@@ -11,18 +11,32 @@ async def send_text():
             audio_file.write(audio_data)
         
 async def send_file():
-    async with websockets.connect("ws://localhost:8000/english/ws/text") as websocket:
+    async with websockets.connect("ws://localhost:8000/english/ws/text/gpu") as websocket:
         file_path = "./test.txt"  # Replace with the path to your text file
         with open(file_path, "r") as file:
             file_content = file.read()
         await websocket.send(f"FILE:{file_content}")
         audio_zip_data = await websocket.recv()
         # Save audio ZIP data to a file
-        with open("audio_files.zip", "wb") as audio_zip_file:
+        with open("audio_files_gpu.zip", "wb") as audio_zip_file:
+            audio_zip_file.write(audio_zip_data)
+
+        
+async def send_file_cpu():
+    async with websockets.connect("ws://localhost:8000/english/ws/text/cpu") as websocket:
+        file_path = "./test.txt"  # Replace with the path to your text file
+        with open(file_path, "r") as file:
+            file_content = file.read()
+        await websocket.send(f"FILE:{file_content}")
+        audio_zip_data = await websocket.recv()
+        # Save audio ZIP data to a file
+        with open("audio_files_cpu.zip", "wb") as audio_zip_file:
             audio_zip_file.write(audio_zip_data)
 
 
-# Test sending text
-asyncio.get_event_loop().run_until_complete(send_file())
 
-asyncio.get_event_loop().run_until_complete(send_text())
+# Test sending text
+# asyncio.get_event_loop().run_until_complete(send_file())
+asyncio.get_event_loop().run_until_complete(send_file_cpu())
+
+# asyncio.get_event_loop().run_until_complete(send_text())
--- websocket-server.py
+++ websocket-server.py
@@ -23,8 +23,6 @@ KIN_MODEL = "./models/"
 
 
 app = FastAPI()
-
-
     
 
 # Function to convert text to sequence
@@ -35,8 +33,15 @@ def get_text(text, hps):
 def generate_audio(text, hps, net_g):
     return get_audio(get_text(text, hps), net_g, hps)
 
+
+def generate_audio_cpu(text, hps, net_g):
+    return get_audio_cpu(get_text(text, hps), net_g, hps)
+
 def process_line(line, hps, net_g):
     return generate_audio(line, hps, net_g)
+
+def process_line_cpu(line, hps, net_g):
+    return generate_audio_cpu(line, hps, net_g)
 
 
 def get_audio(stn_tst,net_g,hps):
@@ -57,8 +62,25 @@ def get_audio(stn_tst,net_g,hps):
     audio_file.seek(0)
     return audio_file.read()
 
+def get_audio_cpu(stn_tst,net_g,hps):
+    with torch.no_grad():
+        x_tst = stn_tst.cpu().unsqueeze(0)
+        x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cpu()
+        audio = net_g.infer(
+            x_tst,
+            x_tst_lengths,
+            noise_scale=0.667,
+            noise_scale_w=0.8,
+            length_scale=1,
+        )[0][0, 0].data.cpu().float().numpy()
 
-@app.websocket("/english/ws/text")
+    audio_file = io.BytesIO()
+    write(audio_file, hps.data.sampling_rate, audio)
+    audio_file.seek(0)
+    return audio_file.read()
+
+
+@app.websocket("/english/ws/text/gpu")
 async def text_to_audio(websocket: WebSocket):
     await websocket.accept()
 
@@ -86,23 +108,73 @@ async def text_to_audio(websocket: WebSocket):
                 file_lines = file_data.split("\n")
 
 
-                
-                # data = CustomData(file_lines, hps)
-                # data_loader = torch.utils.data.DataLoader( data,  batch_size=5,num_workers=os.cpu_count())
-
-                # for idx, (sid, stn_tst) in tqdm(data_loader):
-                #     audio_data = await get_audio(stn_tst,net_g,hps)
-                #     audio_files.append(audio_data)
-                    
-                # for line in file_lines:
-                #     audio_data = get_audio(get_text(line, hps),net_g,hps)
-                #     audio_files.append(audio_data)
-
-                # audio_files.extend(process_lines(file_lines, hps, net_g, max_threads))
-
                 with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
                     # Process each line asynchronously using ThreadPoolExecutor
                     futures = {executor.submit(process_line, line, hps, net_g): line for line in file_lines}
+                    
+                    for future in concurrent.futures.as_completed(futures):
+                        audio_data = future.result()
+                        audio_files.append(audio_data)
+
+                with zipfile.ZipFile("audio_gpu.zip", "w") as zip_file:
+                    for idx, audio_data in enumerate(audio_files):
+                        zip_file.writestr(f"audio_{idx}.wav", audio_data)
+
+                with open("audio_gpu.zip", "rb") as zip_file:
+                    zip_data = zip_file.read()
+                await websocket.send_bytes(zip_data)
+
+                #delete zip file
+                os.remove("audio_gpu.zip")
+
+                break
+                        
+            else:
+                text = data
+
+                audio_data = get_audio(get_text(text, hps),net_g,hps)
+                await websocket.send_bytes(audio_data)
+
+                break
+
+        
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        await websocket.close()
+
+
+@app.websocket("/english/ws/text/cpu")
+async def text_to_audio(websocket: WebSocket):
+    await websocket.accept()
+
+
+    audio_files = []
+    hps = utils.get_hparams_from_file(ENGLISH_CONFIG)
+    net_g = SynthesizerTrn(
+        len(symbols),
+        hps.data.filter_length // 2 + 1,
+        hps.train.segment_size // hps.data.hop_length,
+        **hps.model
+    ).cpu()
+    _ = net_g.eval()
+    _ = utils.load_checkpoint(ENGLISH_MODEL, net_g, None)
+
+    max_threads = os.cpu_count()
+
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if data.startswith("FILE:"):
+                # If the data starts with "FILE:", treat it as a file name
+                file_data = data.replace("FILE:", "")
+                file_lines = file_data.split("\n")
+
+
+                with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+                    # Process each line asynchronously using ThreadPoolExecutor
+                    futures = {executor.submit(process_line_cpu, line, hps, net_g): line for line in file_lines}
                     
                     for future in concurrent.futures.as_completed(futures):
                         audio_data = future.result()
