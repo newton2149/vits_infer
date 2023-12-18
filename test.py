2ad79d31e6a79467b4e968342fb8c9d72733c8cf
Navneeth <navneetsasikumar@gmail.com>
2023-12-18 09:10:02 +0530

    "test"

---
+++ test.py
@@ -0,0 +1,28 @@
+import asyncio
+import websockets
+
+async def send_text():
+    async with websockets.connect("ws://localhost:8000/english/ws/text") as websocket:
+        text = "This is a test sentence."
+        await websocket.send(text)
+        audio_data = await websocket.recv()
+        # Save audio data to a file
+        with open("output_audio.wav", "wb") as audio_file:
+            audio_file.write(audio_data)
+        
+async def send_file():
+    async with websockets.connect("ws://localhost:8000/english/ws/text") as websocket:
+        file_path = "./test.txt"  # Replace with the path to your text file
+        with open(file_path, "r") as file:
+            file_content = file.read()
+        await websocket.send(f"FILE:{file_content}")
+        audio_zip_data = await websocket.recv()
+        # Save audio ZIP data to a file
+        with open("audio_files.zip", "wb") as audio_zip_file:
+            audio_zip_file.write(audio_zip_data)
+
+
+# Test sending text
+asyncio.get_event_loop().run_until_complete(send_file())
+
+asyncio.get_event_loop().run_until_complete(send_text())
